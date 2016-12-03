af261d167dc7eabec7ad25aa5f481a053b18c749
crccheck <c@crccheck.com>
2016-12-03 04:49:37 +0000

    noop when not configured

--- utils/cache.py
+++ utils/cache.py
@@ -1,5 +1,8 @@
 # TODO This module shouldn't really be using a singleton pattern
+import logging
 import os
+
+logger = logging.getLogger(__name__)
 
 
 def get_path(address_components, service):
@@ -11,8 +14,14 @@ def get_path(address_components, service):
 
 
 def get(address_components, service):
-    pass
+    data_dir = os.getenv('DATA_DIR')
+    if not data_dir:
+        logger.debug('Missing DATA_DIR, not retrieving from cache')
+        return
 
 
 def save(address_components, service, data):
-    pass
+    data_dir = os.getenv('DATA_DIR')
+    if not data_dir:
+        logger.debug('Missing DATA_DIR, not saving cache')
+        return
