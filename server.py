c4d0d33be75a56ba23f62f2ac603d9bbfb0ac4ca
crccheck <c@crccheck.com>
2020-07-03 02:23:50 -0500

    delint

--- server.py
+++ server.py
@@ -56,7 +56,7 @@ request_count_cached = Counter(
 
 
 class Lookup(web.View):
-    name = ''
+    name = ""
 
     def get_address(self):
         if {"address", "city", "state", "zip"} - set(self.request.query):
